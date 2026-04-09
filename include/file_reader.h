be35cd5dec066c4ab63b2eacecb3f25bdaf95d4d
SamyMariska <marisikaeric70@gmail.com>
2026-04-09 10:50:51 +0400

    creation de fichier filtre

---
+++ include/file_reader.h
@@ -0,0 +1,6 @@
+#ifndef FILE_READER_H
+#define FILE_READER_H
+
+char** load_words(const char * filename, int *size);
+
+#endif
---
+++ include/filters.h
@@ -0,0 +1,7 @@
+#ifndef FILTERS_H
+#define FILTERS_H
+
+void filter_contains(char **words, int size, char letter);
+void filter_excludes(char **words, int size, char letter);
+
+#endif
